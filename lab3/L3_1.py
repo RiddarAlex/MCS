e38bd66fae7328758cc3b7966817a0ba90c77d26
Alexander Berglund <->
2022-05-04 09:44:05 +0200

    Setting up Lab3 and creating a file for the first exersice

---
+++ lab3/L3_1.py
@@ -0,0 +1,7 @@
+import sys
+from matplotlib import colors
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.colors import ListedColormap
+from matplotlib.pyplot import imshow
