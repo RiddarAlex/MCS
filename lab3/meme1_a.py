98f5b4e8d40a0a697274a084e6b9adbcd009bd8e
RiddarAlex <Ralexb96@gmail.com>
2022-05-16 09:51:56 +0200

    updating lab 3 directory with figures and new code files

--- lab3/meme1_a.py
+++ lab3/meme1_a.py
@@ -6,13 +6,16 @@ import matplotlib.animation as animation
 from matplotlib.colors import ListedColormap
 from matplotlib.pyplot import imshow
 from random import randrange
+import random
+from datetime import datetime
+random.seed()
 
 
-T = 100
+T = 500
 N = 1000
-p = 0.01
-q = 0.05
-r = 0.01
+p = 0.01 #probability of someone resting finding a meme
+q = 0.05 #probablility of a sharer picking a random person
+r = 0.01 #probablility of someone bored picking a random person
 
 # STATES #
 RESTING = 2
@@ -25,10 +28,13 @@ YES = 1
 
 grid = [RESTING for i in range(N)]#np.zeros(N)
 grid[0], grid[-1] = SHARER, BORED
-print(grid)
+# print(grid)
 
 
 newGrid = grid.copy()
+resting_data = [0 for i in range(T)]
+sharer_data = [0 for i in range(T)]
+bored_data = [0 for i in range(T)]
 
 
 for t in range(T):
@@ -38,6 +44,7 @@ for t in range(T):
     for i in range(N):
         if grid[i]  == RESTING:
             newGrid[i] = np.random.choice(states, p=[1-p, p, 0])
+            resting_data[t] += 1
 
         elif grid[i]  == SHARER:
             will_share = np.random.choice([NO, YES], p=[1-q, q])
@@ -47,6 +54,7 @@ for t in range(T):
                     newGrid[target] = SHARER
                 elif grid[target] == BORED:
                     newGrid[i] = BORED
+            sharer_data[t] += 1
 
         elif grid[i]  == BORED:
             will_browse = np.random.choice([NO, YES], p=[1-r, r])
@@ -54,6 +62,8 @@ for t in range(T):
                 target = randrange(N)
                 if grid[target] == RESTING:
                     newGrid[i] = RESTING
+            bored_data[t] += 1
+            
 
     grid = newGrid
 
@@ -61,18 +71,23 @@ for t in range(T):
 
 
 
-    plt.bar(range(N), grid)
-    plt.title(f't = {t}')
+    # plt.bar(range(N), grid)
+    # plt.title(f't = {t}')
 
-    plt.pause(0.001)
-    plt.clf()
+    # plt.pause(0.001)
+    # plt.clf()
+
+# results = np.zeros(T)
+# for t in range(T):
+#     results[t] = np.mean(data[:,t])
+
+# plt.ylabel('Percentage of infected people')
 
 
-results = np.zeros(T)
-for t in range(T):
-    results[t] = np.mean(data[:,t])
-
-
-plt.ylabel('Percentage of infected people')
-
+plt.plot(range(T), resting_data, label="resting")
+plt.plot(range(T), sharer_data, label="sharer")
+plt.plot(range(T), bored_data, label="bored")
+plt.xlabel('t')
+plt.ylabel('Number of occurences')
+plt.legend()
 plt.show()
