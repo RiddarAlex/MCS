dec0d72903b2df7462dc8318c08f7849b5c7c7f5
Alexander Berglund <->
2022-05-31 19:38:04 +0200

    made a version of basic code that is 150 lines

--- project/a_sandbox.py
+++ project/a_sandbox.py
@@ -10,47 +10,31 @@ from matplotlib.pyplot import imshow
 import random
 from random import randrange
 
-counter = 0
-
 N = 32
 
-EMPTY = 1
-UP = 2
-DOWN = 3
-WALL = 9999
-
+EMPTY, UP, DOWN, WALL = 1, 2, 3, 9999
 states = [EMPTY, UP, DOWN]
-NO = 0
-YES = 1
 
 grid = [EMPTY for i in range(N*N)]
 
-grid = np.random.choice(states, (N,N+2), p=[0.9, 0.05, 0.05])
+grid = np.random.choice(states, (N,N+2), p=[0.8, 0.1, 0.1])
 for i in range(N):
     grid[i][0], grid[i][-1] = WALL, WALL
-# grid[0][N//2], grid[-1][N//2] = UP, DOWN
 
 newGrid = grid.copy()
 
 t = 0
 
 # PROBABILITIES
-PL11, PR11, PW11 = 0.25, 0.25, 0.5
-PR12, PW12       = 0.5, 0.5
-PL13, PW13       = 0.5, 0.5
-PL21, PR21, PW21 = 0.25, 0.25, 0.5
-PR22, PW22       = 0.5, 0.5
-PL23, PW23       = 0.5, 0.5
+PL11, PR11, PW11 = 0.25, 0.5, 0.25
+PR12, PW12       = 0.75, 0.25
+PL13, PW13       = 0.25, 0.75
+PL21, PR21, PW21 = 0.25, 0.5, 0.25
+PR22, PW22       = 0.75, 0.25
+PL23, PW23       = 0.25, 0.75
 PB  , PW3        = 0.5, 0.5
 
-
-
-
-
-# np.set_printoptions(threshold=sys.maxsize)
-# print(grid)
-
-
+#-------------------------------------------------------------------------------
 def update(data):
 
     # STATES #
@@ -61,23 +45,19 @@ def update(data):
     global PL11, PR11, PW11, PR12, PW12, PL13, PW13, \
      PL21, PR21, PW21, PR22, PW22, PL23, PW23, PB, PW3
 
+    # FUNCTION INPUT #
+    global newGrid, myType, current
+
     # DOMAIN #
     global grid, t, N
     plt.title(f"t = {t}")
     t += 1
     newGrid = grid.copy()
 
+
     count = 0#N*N
     for i in range(N):
         for j in range(1,N+1):
-
-
-
-            #CHOOSE A NEIGHBOR
-            # target = random.choice(neighbourhood)
-
-            # y_target = (N + (j + target//3 - 1))%N
-            # x_target = (N + (i + target%3  - 1))%N
 
             ### APPLY RULES
             current = (i , j)
@@ -108,80 +88,50 @@ def update(data):
                     newGrid[current] = EMPTY
 
             elif (grid[forward] != EMPTY and grid[left] != EMPTY) and (grid[right] != EMPTY):
-                choices = [backward, current]
-                index = np.random.choice([0, 1], p=[PB, PW3])
-                target = choices[index]
-
-                if newGrid[target] == EMPTY:
-                    newGrid[target] = myType
-                    newGrid[current] = EMPTY
+                move_if_possible([backward, current], [0, 1], [PB, PW3])
 
             elif grid[forward] == myType:
 
                 if (grid[left] == EMPTY) and (grid[right] == EMPTY):
-                    choices = [left, right, current]
-                    index = np.random.choice([0, 1, 2], p=[PL11, PR11, PW11])
-                    target = choices[index]
-
-                    if newGrid[target] == EMPTY:
-                        newGrid[target] = myType
-                        newGrid[current] = EMPTY
+                    move_if_possible([left, right, current], [0, 1, 2], [PL11, PR11, PW11])
 
                 elif (grid[left] != EMPTY) and (grid[right] == EMPTY):
-                    choices = [right, current]
-                    index = np.random.choice([0, 1], p=[PR12, PW12])
-                    target = choices[index]
-
-                    if newGrid[target] == EMPTY:
-                        newGrid[target] = myType
-                        newGrid[current] = EMPTY
+                    move_if_possible([right, current], [0, 1], [PR12, PW12])
 
                 elif (grid[left] == EMPTY) and (grid[right] != EMPTY):
-                    choices = [left, current]
-                    index = np.random.choice([0, 1], p=[PL13, PW13])
-                    target = choices[index]
-
-                    if newGrid[target] == EMPTY:
-                        newGrid[target] = myType
-                        newGrid[current] = EMPTY
-
+                    move_if_possible([left, current], [0, 1], [PL13, PW13])
 
 
             elif grid[forward] == otherType:
 
                 if (grid[left] == EMPTY) and (grid[right] == EMPTY):
-                    choices = [left, right, current]
-                    index = np.random.choice([0, 1, 2], p=[PL21, PR21, PW21])
-                    target = choices[index]
-
-                    if newGrid[target] == EMPTY:
-                        newGrid[target] = myType
-                        newGrid[current] = EMPTY
+                    move_if_possible([left, right, current], [0, 1, 2], [PL21, PR21, PW21])
 
                 elif (grid[left] != EMPTY) and (grid[right] == EMPTY):
-                    choices = [right, current]
-                    index = np.random.choice([0, 1], p=[PR22, PW22])
-                    target = choices[index]
-
-                    if newGrid[target] == EMPTY:
-                        newGrid[target] = myType
-                        newGrid[current] = EMPTY
+                    move_if_possible([right, current], [0, 1], [PR22, PW22])
 
                 elif (grid[left] == EMPTY) and (grid[right] != EMPTY):
-                    choices = [left, current]
-                    index = np.random.choice([0, 1], p=[PL23, PW23])
-                    target = choices[index]
-
-                    if newGrid[target] == EMPTY:
-                        newGrid[target] = myType
-                        newGrid[current] = EMPTY
+                    move_if_possible([left, current], [0, 1], [PL23, PW23])
 
     # update data
     mat.set_data(newGrid)
     grid = newGrid
+    if t%25 == 0:
+        print(f"t: {t} \tPopulation size: {count}")
 
-    print(f"Population size: {count}")
     return [mat]
+
+#-------------------------------------------------------------------------------
+def move_if_possible(choices, indices, probabilities):
+    index = np.random.choice(indices, p=probabilities)
+    target = choices[index]
+
+    if newGrid[target] == EMPTY:
+        newGrid[target] = myType
+        newGrid[current] = EMPTY
+
+#-------------------------------------------------------------------------------
+
 
 # set up animation
 fig, ax = plt.subplots()
